00bc3316bd1a3b151be1b21bd1466d0622b48ecb
maundu55 <marymaundu.mm@gmail.com>
2024-09-16 23:06:48 +0300

    display weather data on page

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const apiKey ="78864d879e25d6f9d32610ba5fb3193f";
-const city = 'dubai'
+// const city = 'nairobi'
+apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=dubai22"
+
+const searchInput = document.querySelector('.search');
+const btnElement = document.getElementById('btn');
+
+// `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
 
 const checkWeather = async () =>{
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
     const data = await response.json();
-    console.log(data);
-    console.log('hi there')
+
+   console.log(data);
+
+   document.querySelector('.city').innerHTML = data.name;
+   document.querySelector('.temp').innerHTML = data.main.temp;
+   document.querySelector('.humidity').innerHTML = data.main.humidity;
+   document.querySelector('.wind').innerHTML = data.wind.speed;
+
 
 }
 checkWeather();
