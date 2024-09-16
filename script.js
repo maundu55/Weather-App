f60cbf07e70a047e15b915c8f6964bca6408951a
maundu55 <marymaundu.mm@gmail.com>
2024-09-16 20:02:21 +0300

    fetch data from api

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const apiKey ="";
-const apiUrl ="";
+const apiKey ="78864d879e25d6f9d32610ba5fb3193f";
+const city = 'dubai'
 
 const checkWeather = async () =>{
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
     const data = await response.json();
     console.log(data);
+    console.log('hi there')
 
 }
 checkWeather();
