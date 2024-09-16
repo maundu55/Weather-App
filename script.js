ddcfc0ffae3742d7cf01a6622149fe83e77d4865
maundu55 <marymaundu.mm@gmail.com>
2024-09-16 23:33:47 +0300

    search city weather dynamically

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const apiKey ="78864d879e25d6f9d32610ba5fb3193f";
-// const city = 'nairobi'
-apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=dubai22"
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
-const searchInput = document.querySelector('.search');
-const btnElement = document.getElementById('btn');
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
 
 // `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
 
-const checkWeather = async () =>{
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const checkWeather = async (city) =>{
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     const data = await response.json();
 
    console.log(data);
 
    document.querySelector('.city').innerHTML = data.name;
-   document.querySelector('.temp').innerHTML = data.main.temp;
-   document.querySelector('.humidity').innerHTML = data.main.humidity;
-   document.querySelector('.wind').innerHTML = data.wind.speed;
-
-
+   document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°c";
+   document.querySelector('.humidity').innerHTML = data.main.humidity +"%";
+   document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
 }
-checkWeather();
+
+searchBtn.addEventListener('click', ()=>{
+   checkWeather(searchBox.value);
+   searchBox.value='';
+})
+
+searchBox.addEventListener("keypress", (event)=>{
+    if(event.key === 'Enter'){
+        event.preventDefault();
+        checkWeather(searchBox.value);
+        searchBox.value='';
+    }
+})
